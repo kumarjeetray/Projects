7c309e7881399508bdb4d4abadc0f194ecdf470c
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-07-07 10:47:18 +0530

    Update forms.py

--- Python/University Website/Version 1.0/forms.py
+++ Python/University Website/Version 1.0/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
-from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from wtforms.validators import Length, EqualTo, DataRequired, ValidationError
 import application
 
 class RegisterForm(FlaskForm):
     def validate_username(self, student_to_check):
 
-        student = application.Student.query.filter_by(username=student_to_check.data).first()
+        student = application.User.query.filter_by(username=student_to_check.data).first()
         if student:
             raise ValidationError('Username already exists! Please try a different username')
 
     def validate_email_address(self, email_address_to_check):
-        email_address = application.Student.query.filter_by(email_address=email_address_to_check.data).first()
+        email_address = application.User.query.filter_by(email_address=email_address_to_check.data).first()
         if email_address:
             raise ValidationError('Email Address already exists! Please try a different email address')
 
@@ -31,3 +31,5 @@ class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
+
+
